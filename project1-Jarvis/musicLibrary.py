59ad27653bb027648d702b622dc3bde9ad77051d
Kavya SP <163286090+Kavyaa13@users.noreply.github.com>
2025-09-18 14:36:02 +0530

    Add files via upload

---
+++ project1-Jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+
+music = {
+    "sky":"https://youtu.be/ZhhBlQC_5N8?si=3RcktJ_c5MGG7Zbt",
+    "monica":"https://youtu.be/2qCpY38ompo?si=jsvElCQ9MDCw-V5B",
+    "wind":"https://youtu.be/FiENDQapd4g?si=UOF6vMV72QrIpmej"
+}
